621ac52da47b75cf14d5b923e9a33affb4cdc9f6
OwenRC0028229 <RC0028229@CRO-24-14>
2018-09-10 15:32:37 +0100

    introduced bugs

--- hello new/Form1.cs
+++ hello new/Form1.cs
@@ -17,10 +17,10 @@ namespace hello_new
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        }
+        private void button1_(object sender, EventArgs e)
         
-            MessageBox.Show("Hello");
+        
+            MessageBoxShow("Hello")
         }
     }
 }
